83ca455343c0265bae62aba9e6049b29e91807ea
Vlada DELL <vlada030@gmail.com>
2021-11-01 21:25:23 +0100

    postavljen layout, zapocet JS

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
 import './scss/main.scss'
+
+
+const subscriptionToggler = document.getElementById('subscriptionToggler')
+const subscriptionSlider = document.getElementById('subscriptionSlider')
+const form = document.forms[0]
+
+const SUBSCRIPTION = new Map([
+    [1, {views: '10K', price: 8}],
+    [2, {views: '50K', price: 12}],
+    [3, {views: '100K', price: 16}],
+    [4, {views: '500K', price: 24}],
+    [5, {views: '1M', price: 36}]
+])
+
+subscriptionSlider.addEventListener('change', () => {
+    const sliderValue = parseInt(subscriptionSlider.value) ;
+    const plan = SUBSCRIPTION.get(sliderValue)
+    console.log({plan});
+})
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    console.log('FORM SUBMITTED...');
+})
+
